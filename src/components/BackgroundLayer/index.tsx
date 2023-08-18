b1993131e8803a90ff2c94a2c2a4344be1e87cb9
Andrey Karev <akarev@broniboy.ru>
2023-08-18 15:39:34 +0200

    FEATURE change bg image

--- src/components/BackgroundLayer/index.tsx
+++ src/components/BackgroundLayer/index.tsx
@@ -1,18 +1,20 @@
 import { Stage, Image, Layer } from "react-konva";
 import PlayerLayer from "components/PlayerLayer";
 import useImage from "use-image";
+import bg2 from "assets/BG2.svg";
 import bg1 from "assets/BG1.svg";
 
 function BackgroundLayer() {
-  const [srcImg] = useImage(bg1);
+  const [bg1Src] = useImage(bg1);
+  const [bg2Src] = useImage(bg2);
+  //ЗАКОНЧИЛ НА ОФСЕТЕ
+
+  if (!bg1Src) return;
 
   return (
-    <Layer>
-      <Image
-        image={srcImg}
-        width={window.innerWidth}
-        height={window.innerHeight}
-      />
+    <Layer offsetX={(window.innerWidth - bg1Src.width) / 2}>
+      <Image image={bg1Src} />
+      <Image image={bg2Src} />
     </Layer>
   );
 }
